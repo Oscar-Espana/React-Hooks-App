9517eef8735d510f93074856bdadd7e0267a1ab4
Oscar España <espaaoscar@gmail.com>
2020-11-21 18:15:37 -0500

    Update the form using Custom Hook - useEffect, useState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { SimpleForms } from "./components/02-useEffect/SimpleForms";
+import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
+// import { SimpleForms } from "./components/02-useEffect/SimpleForms";
 // import { HookApp } from "./HookApp";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<SimpleForms />, document.getElementById("root"));
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
