832290b475e26ae2315c7ac5637e5a65db058212
Oscar España <espaaoscar@gmail.com>
2020-11-21 17:39:29 -0500

    Create a form with useEffect hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SimpleForms } from "./components/02-useEffect/SimpleForms";
 // import { HookApp } from "./HookApp";
 // import { CounterApp } from "./components/01-useState/CounterApp";
-import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+// import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<CounterWithCustomHook />, document.getElementById("root"));
+ReactDOM.render(<SimpleForms />, document.getElementById("root"));
