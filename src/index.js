6324de480954cb5db3922d86459e2bfa1622d55c
Oscar España <espaaoscar@gmail.com>
2020-11-22 17:39:11 -0500

    Example using useReducer Hook - create the layout

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { TodoApp } from "./components/08-useReducer/TodoApp";
 // import { Padre } from "./components/07-tarea-memo/Padre";
-// // import { MemoHook } from "./components/06-memos/MemoHook";
-// // import { Memorize } from "./components/06-memos/Memorize";
-// // import { Layout } from "./components/05-useLayoutEffect/Layout";
-// // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
-// // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
-// // import { FocusScreen } from "./components/04-useRef/FocusScreen";
-// // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
-// // import { SimpleForms } from "./components/02-useEffect/SimpleForms";
-// // import { HookApp } from "./HookApp";
-// // import { CounterApp } from "./components/01-useState/CounterApp";
-// // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+// import { MemoHook } from "./components/06-memos/MemoHook";
+// import { Memorize } from "./components/06-memos/Memorize";
+// import { Layout } from "./components/05-useLayoutEffect/Layout";
+// import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+// import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// import { FocusScreen } from "./components/04-useRef/FocusScreen";
+// import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
+// import { SimpleForms } from "./components/02-useEffect/SimpleForms";
+// import { HookApp } from "./HookApp";
+// import { CounterApp } from "./components/01-useState/CounterApp";
+// import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-// ReactDOM.render(<Padre />, document.getElementById("root"));
-
-import "./components/08-useReducer/intro-reducer";
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
