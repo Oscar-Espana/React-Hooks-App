5aeb73b39558cc7faa9ae5e88d2402407ac5c47e
Oscar España <espaaoscar@gmail.com>
2020-11-22 09:17:41 -0500

    Exaple using React.memo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Layout } from "./components/05-useLayoutEffect/Layout";
+import { Memorize } from "./components/06-memos/Memorize";
+// import { Layout } from "./components/05-useLayoutEffect/Layout";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
@@ -10,4 +11,4 @@ import { Layout } from "./components/05-useLayoutEffect/Layout";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<Layout />, document.getElementById("root"));
+ReactDOM.render(<Memorize />, document.getElementById("root"));
