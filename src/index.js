bc8c0041026a604aa79af0dedc3c0560d84d0568
Oscar España <espaaoscar@gmail.com>
2020-11-21 23:09:45 -0500

    Using the useRef hook in a real case

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
-import { FocusScreen } from "./components/04-useRef/FocusScreen";
+// import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 // import { SimpleForms } from "./components/02-useEffect/SimpleForms";
 // import { HookApp } from "./HookApp";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<FocusScreen />, document.getElementById("root"));
+ReactDOM.render(<RealExampleRef />, document.getElementById("root"));
