47b9317e942468cc6126dabc25fde4c8b52e3a5f
Oscar España <espaaoscar@gmail.com>
2020-11-22 09:41:47 -0500

    Example using the useMemo hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Memorize } from "./components/06-memos/Memorize";
+import { MemoHook } from "./components/06-memos/MemoHook";
+// import { Memorize } from "./components/06-memos/Memorize";
 // import { Layout } from "./components/05-useLayoutEffect/Layout";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
@@ -11,4 +12,4 @@ import { Memorize } from "./components/06-memos/Memorize";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<Memorize />, document.getElementById("root"));
+ReactDOM.render(<MemoHook />, document.getElementById("root"));
