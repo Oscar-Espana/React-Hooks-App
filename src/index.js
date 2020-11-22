cd599ead0127192a9c8aa84a5b548e1f6da1e6c8
Oscar España <espaaoscar@gmail.com>
2020-11-22 13:30:12 -0500

    Intro to reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Padre } from "./components/07-tarea-memo/Padre";
-// import { MemoHook } from "./components/06-memos/MemoHook";
-// import { Memorize } from "./components/06-memos/Memorize";
-// import { Layout } from "./components/05-useLayoutEffect/Layout";
-// import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
-// import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
-// import { FocusScreen } from "./components/04-useRef/FocusScreen";
-// import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
-// import { SimpleForms } from "./components/02-useEffect/SimpleForms";
-// import { HookApp } from "./HookApp";
-// import { CounterApp } from "./components/01-useState/CounterApp";
-// import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Padre } from "./components/07-tarea-memo/Padre";
+// // import { MemoHook } from "./components/06-memos/MemoHook";
+// // import { Memorize } from "./components/06-memos/Memorize";
+// // import { Layout } from "./components/05-useLayoutEffect/Layout";
+// // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+// // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// // import { FocusScreen } from "./components/04-useRef/FocusScreen";
+// // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
+// // import { SimpleForms } from "./components/02-useEffect/SimpleForms";
+// // import { HookApp } from "./HookApp";
+// // import { CounterApp } from "./components/01-useState/CounterApp";
+// // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<Padre />, document.getElementById("root"));
+// ReactDOM.render(<Padre />, document.getElementById("root"));
+
+import "./components/08-useReducer/intro-reducer";
