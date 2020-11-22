86b99f3a7e94a3a43c45db5401cfc105a1bea3d6
Oscar España <espaaoscar@gmail.com>
2020-11-22 10:24:41 -0500

    task on using hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MemoHook } from "./components/06-memos/MemoHook";
+import { Padre } from "./components/07-tarea-memo/Padre";
+// import { MemoHook } from "./components/06-memos/MemoHook";
 // import { Memorize } from "./components/06-memos/Memorize";
 // import { Layout } from "./components/05-useLayoutEffect/Layout";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
@@ -12,4 +13,4 @@ import { MemoHook } from "./components/06-memos/MemoHook";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<MemoHook />, document.getElementById("root"));
+ReactDOM.render(<Padre />, document.getElementById("root"));
