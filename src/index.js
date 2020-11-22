a0573237fa70c10a503be3b496a56ab95e8eaf04
Oscar España <espaaoscar@gmail.com>
2020-11-21 22:43:02 -0500

    Basic example of useRef hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 // import { SimpleForms } from "./components/02-useEffect/SimpleForms";
 // import { HookApp } from "./HookApp";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 
-ReactDOM.render(<MultipleCustomHooks />, document.getElementById("root"));
+ReactDOM.render(<FocusScreen />, document.getElementById("root"));
