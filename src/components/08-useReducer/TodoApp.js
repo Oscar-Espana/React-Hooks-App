2bff3ac1929daea0492aed2af4ab0029f23fa97f
Oscar España <espaaoscar@gmail.com>
2020-11-22 18:44:02 -0500

    Add an action with useReducer

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -11,7 +11,24 @@ const initialState = [
 ];
 
 export const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: "Nueva tarea ",
+      done: false,
+    };
+
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
+
+    dispatch(action);
+  };
 
   return (
     <div>
@@ -34,7 +51,7 @@ export const TodoApp = () => {
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               name="description"
@@ -42,7 +59,10 @@ export const TodoApp = () => {
               autoComplete="off"
               className="form-control"
             />
-            <button className="btn btn-outline-primary mt-1 btn-block">
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1 btn-block"
+            >
               Agregar
             </button>
           </form>
