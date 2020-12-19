2ab7390efe61e563ef0eb586814404eacdc3a52c
Oscar España <espaaoscar@gmail.com>
2020-12-18 22:38:05 -0500

    using: createContext and useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,19 @@
 import React from "react";
 import AppRouter from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 const HomeScreen = () => {
-  return <AppRouter />;
+  const user = {
+    id: 1234,
+    name: "Oscar Espana",
+    email: "espaaoscar@gmail.com",
+  };
+
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
 
 export default HomeScreen;
