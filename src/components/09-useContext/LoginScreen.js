727f1d046eb39aeb92d135bd43c47fb6b67f2aa9
Oscar España <espaaoscar@gmail.com>
2020-12-18 22:57:50 -0500

    Create a simple login and logout with useContext

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 const HomeScreen = () => {
+  const { setUser } = useContext(UserContext);
+
   return (
     <div>
       <h1>Login Screen </h1>
       <hr />
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          setUser({
+            id: 1234,
+            name: "Oscar España",
+          })
+        }
+      >
+        Login
+      </button>
     </div>
   );
 };
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./AppRouter";
 import { UserContext } from "./UserContext";
 
 const HomeScreen = () => {
-  const user = {
-    id: 1234,
-    name: "Oscar Espana",
-    email: "espaaoscar@gmail.com",
-  };
+  const [user, setUser] = useState({});
 
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
       <AppRouter />
     </UserContext.Provider>
   );
